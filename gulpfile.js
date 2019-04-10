07a8ea5ff04f4f464bdf05f0657245778b667bd7
Mitch Samuels <samuels.mitch@gmail.com>
2019-04-09 19:21:17 -0500

    Changed folder structure and some gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,20 @@ var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
 var del = require('del');
 var runSequence = require('run-sequence');
-var cacheBuster = require('gulp-cache-bust');
-var purgecss = require('gulp-purgecss');
 var replace = require('gulp-replace');
 var less = require('gulp-less');
 var browserSync = require("browser-sync").create();
 var reload = browserSync.reload;
+var newer = require('gulp-newer');
+var imagemin = require('gulp-imagemin');
+
+var folder = {
+        src: 'app/',
+        build: 'build/'
+    }
 
 
-///////////////////////
-// Minify CSS and JS //
-///////////////////////
+// Minify CSS and JS
 gulp.task('useref', function(){
     return gulp.src('app/*.html')
         .pipe(useref())
@@ -26,77 +29,58 @@ gulp.task('useref', function(){
         // Minifies only if it's a CSS file
         .pipe(gulpIf('*.css', cssnano()))
 
-        .pipe(gulp.dest('public'))
+        .pipe(gulp.dest('build'))
 });
 
-////////////////////////
-// Clean build folder //
-////////////////////////
+
+// Clean build folder
 gulp.task('clean:dist', function() {
-    return del.sync('public');
+    return del.sync('build');
 })
 
-///////////////////////
-// Move static files //
-///////////////////////
+// Move static files
 var filesToMove = [
-        './app/assets/**/**/*',
         './app/manifest.json'
     ];
 gulp.task('static', function(){
     gulp.src(filesToMove, { base: './app/' })
-        .pipe(gulp.dest('public'));
+        .pipe(gulp.dest('build'));
 });
 
-
-//////////////////
-// Cache Buster //
-//////////////////
-gulp.task('cacheBuster', function () {
-    return gulp.src('public/index.html')
-        .pipe(cacheBuster())
-        .pipe(gulp.dest('public'));
-});
-
-//////////////////////
-// Purge unused CSS //
-//////////////////////
-gulp.task('purgeCSS', function () {
-    return gulp.src('public/css/main.css')
-        .pipe(purgecss({
-            content: ["public/**/*.html"]
-        }))
-        .pipe(gulp.dest('public/css'))
+// image processing
+gulp.task('images', function () {
+    var out = folder.build + 'src/';
+    return gulp.src(folder.src + 'src/**/*')
+        .pipe(newer(out))
+        .pipe(imagemin({ optimizationLevel: 5 }))
+        .pipe(gulp.dest(out));
 });
 
 
 ////////////////////
 // Build sequence //
 ////////////////////
+
 // Clean first, then the rest
 gulp.task('build', function (callback) {
     runSequence(
         'clean:dist',
-        ['useref', 'static'],
+        ['useref', 'static', 'images'],
         // ['useref', 'static', 'purgeCSS'],
-        ['cacheBuster'],
         callback
     )
 })
 
 
-
-/////////////////
-// Browsersync //
-/////////////////
+// Browsersync
 // Watch for less changes
 // Compile less on save
 // inject CSS to browser
 // Live reload for HTML and JS
 gulp.task('compile-less', function () {
-    gulp.src('./app/stylesheets/less/main.less')
+    gulp.src('./app/less/main.less')
         .pipe(less())
-        .pipe(gulp.dest('./app/stylesheets/css/'))
+        .pipe(gulp.dest('./app/'))
         .pipe(browserSync.stream());
 }); 
 
@@ -111,7 +95,7 @@ gulp.task('serve', function () {
         server: "./app/"
     });
 
-    gulp.watch("app/stylesheets/less/**/*.less", ['compile-less']);
+    gulp.watch("app/less/**/*.less", ['compile-less']);
     gulp.watch("./app/**/*.html").on("change", reload);
     gulp.watch("./app/**/*.js").on("change", reload);
 });
@@ -130,7 +114,7 @@ gulp.task('serve-build', function () {
 
     // Serve files from the root of this project
     browserSync.init({
-        server: "./public/"
+        server: "./build/"
     });
 
 });
